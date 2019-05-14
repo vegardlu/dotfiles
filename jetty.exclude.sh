3bf65ae93b6b32fc925b422950ad2e5b9879eea2
Vegard Hillestad <vegard.hillestad@evry.com>
2019-05-14 19:56:55 +0200

    make jetty folder if it does not exists

--- jetty.exclude.sh
+++ jetty.exclude.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
-rm -rv ~/jetty/template 
+rm -rv ~/jetty/template
+mkdir -v ~/jetty 
 mkdir -v ~/jetty/template 
 cp -vf srv-adm-jetty.exclude.sh ~/jetty/template/srv-adm.sh
 cp -vf start-jetty.exclude.ini ~/jetty/template/start.ini
