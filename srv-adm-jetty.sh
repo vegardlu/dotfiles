a642a7cd9643120946ce70165d4e699ab2bde1d6
Vegard Lundeberg Hillestad <vegard.hillestad@evry.com>
2021-02-18 13:22:36 +0100

    remove spring profile override

--- srv-adm-jetty.sh
+++ srv-adm-jetty.sh
@@ -4,6 +4,6 @@ export JETTY_SH=$JETTY_HOME"/bin/jetty.sh"
 export app=LOCAL
 export logstashDestination=LOGSTASH_DESTINATION 
 export JETTY_BASE=`pwd`
-export JAVA_OPTIONS="-Xmx2g -Dspring.profiles.active=adcm,default"
+export JAVA_OPTIONS="-Xmx2g"
  
 sh $JETTY_SH $1 JAVA=$JAVA JAVA_OPTIONS=$JAVA_OPTIONS JETTY_HOME=$JETTY_HOME JETTY_BASE=$JETTY_BASE JETTY_RUN=$JETTY_BASE
